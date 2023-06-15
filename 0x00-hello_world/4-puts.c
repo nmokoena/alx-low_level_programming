6a3d491157b77334493b7b4bb331ec0755519b31
nmokoena <naledimoteka2@gmail.com>
2023-06-14 23:24:58 -0700

    2-assembler

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
  *main - Entry point
 
- *reurn : Always 0 (success
+ *reurn : Always 0 (success)
  */
 int main(void)
-{  
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
-
