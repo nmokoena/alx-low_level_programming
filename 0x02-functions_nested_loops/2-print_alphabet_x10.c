cf0a73f5d72cfd7835c3dc8d5cc93085962a8e77
nmokoena <naledimoteka2@gmail.com>
2023-06-20 09:41:33 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,16 +6,16 @@
  */
 void print_alphabet_x10(void)
 {
-    char c;
-    int i = 0;
+char c;
+int i = 0;
 
-    while (i <= 0)
-    {
-    for (c = 'a'; c <= 'z'; c++)
-    {
-    _putchar(c);
-    }
-    _putchar('\n');
-    i++;
-    }
+while (i <= 0)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+i++;
+}
 }
