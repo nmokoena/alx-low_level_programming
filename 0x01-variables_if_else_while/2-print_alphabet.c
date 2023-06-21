4ec9f3c01fc8e62425732d7cb040ea0b41fb87b6
nmokoena <naledimoteka2@gmail.com>
2023-06-21 13:27:13 -0700

    2 digit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+int lowercase 'a';
+while (lowerCase <= 'z')
+{
+putchar(lowerCase);
+lowerCase += 1;
+}
+putchar('\n');
+return (0);
+}
