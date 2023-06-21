84612aef9c8ae586fb38ef2c20c109ecff24f22a
nmokoena <naledimoteka2@gmail.com>
2023-06-21 13:13:04 -0700

    alp

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
