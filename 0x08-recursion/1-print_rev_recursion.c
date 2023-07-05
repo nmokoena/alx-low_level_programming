c905fb114591f5e9b3a5043d07e9e6daa3aa0bb5
nmokoena <naledimoteka2@gmail.com>
2023-07-05 06:50:01 -0700

    rev

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _print_rev_recursions - Print a string in reverse
+ * _print_rev_recursion - Print a string in reverse
  * @s: the string to reverse
  *
  * Return: nothing
